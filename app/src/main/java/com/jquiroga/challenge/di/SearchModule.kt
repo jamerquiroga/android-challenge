c1b52569f4fbfd9a7a0daeaf311a64f33ef6f448
Jamer Quiroga <jamer.quiroga@avantica.com>
2021-04-07 22:54:28 -0500

    added use case to save search term

--- app/src/main/java/com/jquiroga/challenge/di/SearchModule.kt
+++ app/src/main/java/com/jquiroga/challenge/di/SearchModule.kt
@@ -4,6 +4,7 @@ import com.jquiroga.data.datasource.local.source.SearchLocalDataSource
 import com.jquiroga.data.mapper.search.SearchLocalMapper
 import com.jquiroga.data.repository.SearchDataRepository
 import com.jquiroga.domain.repository.SearchRepository
+import com.jquiroga.domain.usecase.SaveSearchTermUseCase
 import org.koin.dsl.module
 
 val searchModule = module {
@@ -17,4 +18,6 @@ val searchModule = module {
     single<SearchRepository> {
         SearchDataRepository(searchLocalDataSource = get(), searchLocalMapper = get())
     }
+
+    factory { SaveSearchTermUseCase(searchRepository = get()) }
 }
