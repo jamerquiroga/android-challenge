b061ce35b2e819597bd7344f878843c7d9f6d42b
Jamer Quiroga <jamer.quiroga@avantica.com>
2021-04-07 22:15:39 -0500

    updated search dao

--- data/src/main/java/com/jquiroga/data/datasource/local/db/dao/SearchDao.kt
+++ data/src/main/java/com/jquiroga/data/datasource/local/db/dao/SearchDao.kt
@@ -3,6 +3,7 @@ package com.jquiroga.data.datasource.local.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.jquiroga.data.datasource.local.db.entity.SearchRoom
 
 @Dao
@@ -10,4 +11,7 @@ interface SearchDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(value: SearchRoom)
+
+    @Query("SELECT * FROM SearchRoom")
+    suspend fun getAllSearch(): List<SearchRoom>
 }
