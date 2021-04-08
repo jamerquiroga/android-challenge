a7ec0599515648f4eaa4a27a30e044189c9d9e4a
Jamer Quiroga <jamer.quiroga@avantica.com>
2021-04-08 03:55:44 -0500

    added unit tests for SearchHistoryViewModel

--- app/src/main/java/com/jquiroga/challenge/features/searchhistory/view/SearchHistoryViewState.kt
+++ app/src/main/java/com/jquiroga/challenge/features/searchhistory/view/SearchHistoryViewState.kt
@@ -4,5 +4,5 @@ import com.jquiroga.challenge.core.failure.FailureModel
 
 sealed class SearchHistoryViewState {
     class SuccessGetSearchHistory(val searchHistory: List<String>) : SearchHistoryViewState()
-    class Failure(val failure: FailureModel) : SearchHistoryViewState()
+        class Failure(val failure: FailureModel) : SearchHistoryViewState()
 }
---
+++ app/src/test/java/com/jquiroga/challenge/provider/SearchHistoryDataProvider.kt
@@ -0,0 +1,12 @@
+package com.jquiroga.challenge.provider
+
+object SearchHistoryDataProvider {
+
+    fun searchHistoryDummy () = listOf(
+        "Guns n roses",
+        "U2",
+        "Aerosmith",
+        "Velvet Revolver",
+        "AudiosLave"
+    )
+}
